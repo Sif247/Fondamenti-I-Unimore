befe6e35f8c9b80085ddd508f3659c2f3fca04d9
Ismail Sifdine <131058788+Sif247@users.noreply.github.com>
2023-12-14 12:56:41 +0100

    conversione.c

--- ESERCITAZIONE 3/es3_5/conversione.c
+++ ESERCITAZIONE 3/es3_5/conversione.c
@@ -1,26 +1,26 @@
 #include <stdlib.h>
 
-char* converti(unsigned int n)
-{
-	if (n == 0 || n == NULL)
-	{
-		return NULL;
-	}
+char* converti(unsigned int n) {
+    
+    unsigned int temp = n;
+    int num_cifre = 0;
+    while (temp != 0) {
+        temp /= 10;
+        num_cifre++;
+    }
 
-	unsigned int size = 0;
-	unsigned int copia = n;
-	while (n > 0)
-	{
-		n = n / 10;
-		size++;
-	}
-	char* ris = malloc(size * sizeof(char) + 1);
-	ris[size] = 0;
+    
+    char* result = (char*)malloc((num_cifre + 1) * sizeof(char)); // +1 per il terminatore '\0'
+    if (result == NULL) {
+        return NULL; 
+    }
 
-	for (int i = size; i > 0; i--) {
-		ris[size - i] = copia % 10 + '0';
-		copia /= 10;
-	}
-	return ris;
+    
+    for (int i = num_cifre - 1; i >= 0; i--) {
+        result[i] = (char)((n % 10) + '0');
+        n /= 10;
+    }
+    result[num_cifre] = '\0';
 
+    return result;
 }
